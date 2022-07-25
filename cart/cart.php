fbba4884ed3048acc8d8d616fc9d017ff512eb92
James Lay <james@eventrentalsystems.com>
2022-07-24 21:45:53 -0600

    add about, contact, policy, and cart

---
+++ cart/cart.php
@@ -0,0 +1,69 @@
+<style><?php require_once __DIR__ . "/" . "styles.css";?></style>
+<script><?php require_once __DIR__ . "/" . "script.js";?></script>
+<section class="cart">
+    <!-- TODO: show all items and do the checkout process here -->
+    <h1>Shopping Cart - 3 items <span class="X" onclick="cartX()">X</span></h1>
+    <div id="cart-container">
+
+        <ul>
+
+            <li>
+                <div class="image" style="background-image: url('/store/images/pexels-cottonbro-4155435.jpg');"></div>
+                <div class="info">
+                    <h2>
+                        Clarity
+                        <i class="X" onclick="itemX(itemid)">X</i>
+                    </h2>
+                    <div class="price">$15.98</div>
+                    <div class="quantity">
+                        <button onclick="cartMinus(itemid)">-</button>
+                        <span>1</span>
+                        <button onclick="cartPlus(itemid)">+</button>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="image" style="background-image: url('/store/images/pexels-cottonbro-4155435.jpg');"></div>
+                <div class="info">
+                    <h2>
+                        Clarity
+                        <i class="X" onclick="itemX(itemid)">X</i>
+                    </h2>
+                    <div class="price">$15.98</div>
+                    <div class="quantity">
+                        <button onclick="cartMinus(itemid)">-</button>
+                        <span>1</span>
+                        <button onclick="cartPlus(itemid)">+</button>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="image" style="background-image: url('/store/images/pexels-cottonbro-4155435.jpg');"></div>
+                <div class="info">
+                    <h2>
+                        Clarity
+                        <i class="X" onclick="itemX(itemid)">X</i>
+                    </h2>
+                    <div class="price">$15.98</div>
+                    <div class="quantity">
+                        <button onclick="cartMinus(itemid)">-</button>
+                        <span>1</span>
+                        <button onclick="cartPlus(itemid)">+</button>
+                    </div>
+                </div>
+            </li>
+
+        </ul>
+
+        <div id="cart-total">
+            <div><div>Subtotal</div>          <div>$15.98</div></div>
+            <div><div>Standard Shipping</div> <div>FREE</div></div>
+            <div><div>Total</div>             <div>$15.98</div></div>
+        </div>
+
+        <button id="checkout-btn">Checkout</button>
+
+    </div>
+</section>
---
+++ cart/script.js
@@ -0,0 +1,12 @@
+window.cartX = function() {
+    // TODO - close the cart window
+};
+window.itemX = function(item) {
+    // TODO - remove the item from the cart
+}
+window.cartMinus = function(item) {
+    // TODO - decrease the quantity of the item in the cart
+}
+window.cartPlus = function(item) {
+    // TODO - increase the quantity of the item in the cart
+}
