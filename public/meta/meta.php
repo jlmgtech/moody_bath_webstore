1c1f650aee41bb7f9f733928bfce3931972a2cb8
james lay <james@eventrentalsystems.com>
2022-09-06 14:54:22 -0600

    prevent weird x-scrolling

--- public/meta/meta.php
+++ public/meta/meta.php
@@ -1,5 +1,11 @@
-        <meta charset="utf-8" />
-        <meta name="author" content="" />
-        <meta name="keywords" content="" />
-        <meta name="description" content="" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="utf-8" />
+<meta name="author" content="" />
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<style>
+body {
+    width: 100vw;
+    overflow-x: hidden;
+}
+</style>
